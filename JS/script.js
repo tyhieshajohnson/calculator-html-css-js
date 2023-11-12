f064b65be757da9e68c071960dced062bb07db1a
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 01:21:10 +0200

    javaScriptButtons

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,14 @@
 // create a display with javascript
-let display = document.getElementById('display')
-console.log('display');
+let display = document.getElementById('display');
 
-// create functioning for buttons
-let calButton = document.querySelector('#equal')
-let calButton = document.querySelector('#addition')
-let calButton = document.querySelector('#subtraction')
-let calButton = document.querySelector('#division')
+// group all buttons
+let calButton = Array.from(document.getElementsByClassName('calButton'));
+
+calButton.map( calButton => {
+    calButton.addEventListener('click', (e) => {
+        console.log('clicked');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+    });
+});
