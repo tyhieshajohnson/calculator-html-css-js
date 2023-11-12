3cc4958f39f044f0053f893726cb3e6262aba358
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 01:54:49 +0200

    javaScriptClearButton

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,12 @@ let calButton = Array.from(document.getElementsByClassName('calButton'));
 calButton.map( calButton => {
     calButton.addEventListener('click', (e) => {
         switch(e.target.innerText){
+            case 'CA':
+                display.innerText = '';
+                break;
+            case '=':
+                display.innerText = eval(display.innerText);
+                break;
             default:
                 display.innerText += e.target.innerText;
         }
