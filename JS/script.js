752d81f936fa1c00a2ca64d7052cfe5a5a3f4793
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 00:45:18 +0200

    buttonLabel

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
 // create a display with javascript
 let display = document.getElementById('display')
 console.log('display');
+
+// create functioning for buttons
+let calButton = document.querySelector('#equal')
+let calButton = document.querySelector('#addition')
+let calButton = document.querySelector('#subtraction')
+let calButton = document.querySelector('#division')
