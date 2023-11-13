1d348133b8805bbeec3c2401d28347558eaf9d68
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 11:30:56 +0200

    javaScript

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,32 @@
-let display = document.getElementById('[display]');
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the display input
+    let displayInput = document.getElementById('calInput');
+
+    // Get all buttons
+    let buttons = document.querySelectorAll('.calButton button');
+
+    // Add click event listeners to all buttons
+    buttons.forEach(button => {
+        button.addEventListener('click', function (e) {
+            updateDisplay(e.target.innerText);
+        });
+    });
+
+    // Function to update the display based on button clicks
+    function updateDisplay(value) {
+        switch (value) {
+            case 'CA':
+                displayInput.value = '';
+                break;
+            case '=':
+                try {
+                    displayInput.value = eval(displayInput.value);
+                } catch (error) {
+                    displayInput.value = 'Error';
+                }
+                break;
+            default:
+                displayInput.value += value;
+        }
+    }
+});
