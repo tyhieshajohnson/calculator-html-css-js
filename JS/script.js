f77b77656d0014932e0bbab9a1a3f86cd1f173e4
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 09:56:55 +0200

    titleChange

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,9 @@ let display = document.getElementById('display');
 // group all buttons
 let calButton = Array.from(document.getElementsByClassName('calButton'));
 
-calButton.map( calButton => {
+calButton.map(calButton => {
     calButton.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+        switch (e.target.innerText) {
             case 'CA':
                 display.innerText = '';
                 break;
@@ -14,7 +14,13 @@ calButton.map( calButton => {
                 display.innerText = eval(display.innerText);
                 break;
             default:
-                display.innerText += e.target.innerText;
+                // Check if the last character is a number before appending
+                if (!isNaN(display.innerText.slice(-1))) {
+                    display.innerText += e.target.innerText;
+                } else {
+                    // If the last character is not a number, replace it with the new one
+                    display.innerText = display.innerText.slice(0, -1) + e.target.innerText;
+                }
         }
     });
 });
